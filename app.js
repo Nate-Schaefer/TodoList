09a46270ae025bd69fb72db7432a2591ab9d5643
Nate-Schaefer <nathanschaefer-schaefer@uiowa.edu>
2023-05-02 17:32:44 -0500

    put html and css files in public folder, used express to connect html and css files in localhost

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/index.html', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+
+app.get('/public/index.html', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
   });
   
 
